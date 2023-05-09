f3706c900a758900e490283aea508e49959545c9
shijito <zero.seiyuu@gmail.com>
2023-05-09 15:17:44 +0900

    確認のため

--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div class="text-right w-75 m-auto">
-      <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts">
+      <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts"> 
     </div>
   </div>
 </div>
