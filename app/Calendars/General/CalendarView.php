e848ac169feb2ce2a899f730308ec364d49714c2
shijito <zero.seiyuu@gmail.com>
2023-05-09 17:03:27 +0900

    受付終了表示追加

--- app/Calendars/General/CalendarView.php
+++ app/Calendars/General/CalendarView.php
@@ -57,12 +57,14 @@ class CalendarView{
             $reservePart = "リモ3部";
           }
           if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
-            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'参加</p>';//何部に参加しているかを表示する
+            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px" >'. $reservePart .'参加</p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }else{
             $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }
+        }else if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+          $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px" >受付終了</p>';
         }else{
           $html[] = $day->selectPart($day->everyDay());
         }
